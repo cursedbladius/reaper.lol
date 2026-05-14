3f5708ab5dce77d4447337e26a2b1cbd6e4855d3
199 <cursedbladi@gmail.com>
2026-05-15 01:35:41 +0300

    Update main.lua

--- public/main.lua
+++ public/main.lua
@@ -1,14 +1,1 @@
-print("START")
-
-local library = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/i77lhm/vaderpaste/refs/heads/main/library.lua"
-))()
-
-print("LIBRARY LOADED")
-
-local window = library:window({
-    name = "reaper.lol",
-    size = UDim2.fromOffset(500, 650)
-})
-
-print("WINDOW CREATED")
+print("REAPER TEST")
