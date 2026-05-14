9f9c4237a017ca20bea4db2285238778e6361146
199 <cursedbladi@gmail.com>
2026-05-15 01:32:36 +0300

    Update main.lua

--- public/main.lua
+++ public/main.lua
@@ -1,112 +1,14 @@
-local Players = game:GetService("Players")
-local Camera = workspace.CurrentCamera
+print("START")
 
-local LocalPlayer = Players.LocalPlayer
+local library = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/i77lhm/vaderpaste/refs/heads/main/library.lua"
+))()
 
-local ESP = {}
-ESP.Objects = {}
+print("LIBRARY LOADED")
 
-function ESP:Init(flagsTable)
-    self.Flags = flagsTable
-end
+local window = library:window({
+    name = "reaper.lol",
+    size = UDim2.fromOffset(500, 650)
+})
 
-function ESP:Create(player)
-
-    if self.Objects[player] then
-        return
-    end
-
-    local box = Drawing.new("Square")
-    box.Visible = false
-    box.Filled = false
-    box.Thickness = 1
-
-    local name = Drawing.new("Text")
-    name.Visible = false
-    name.Center = true
-    name.Outline = true
-    name.Size = 13
-
-    self.Objects[player] = {
-        Box = box,
-        Name = name
-    }
-
-end
-
-function ESP:Hide()
-
-    for _, drawings in pairs(self.Objects) do
-        drawings.Box.Visible = false
-        drawings.Name.Visible = false
-    end
-
-end
-
-function ESP:Update()
-
-    for _, player in pairs(Players:GetPlayers()) do
-
-        if player == LocalPlayer then
-            continue
-        end
-
-        local character = player.Character
-
-        if not character then
-            continue
-        end
-
-        local root = character:FindFirstChild("HumanoidRootPart")
-        local humanoid = character:FindFirstChild("Humanoid")
-
-        if not root or not humanoid then
-            continue
-        end
-
-        if humanoid.Health <= 0 then
-            continue
-        end
-
-        self:Create(player)
-
-        local drawings = self.Objects[player]
-
-        local position, visible = Camera:WorldToViewportPoint(root.Position)
-
-        if not visible then
-            drawings.Box.Visible = false
-            drawings.Name.Visible = false
-            continue
-        end
-
-        local width = 60
-        local height = 100
-
-        -- BOX
-        drawings.Box.Size = Vector2.new(width, height)
-
-        drawings.Box.Position = Vector2.new(
-            position.X - width / 2,
-            position.Y - height / 2
-        )
-
-        drawings.Box.Color = Color3.fromRGB(255, 0, 0)
-        drawings.Box.Visible = self.Flags.visuals_boxes
-
-        -- NAME
-        drawings.Name.Text = player.Name
-
-        drawings.Name.Position = Vector2.new(
-            position.X,
-            position.Y - height / 2 - 15
-        )
-
-        drawings.Name.Color = Color3.fromRGB(255, 255, 255)
-        drawings.Name.Visible = self.Flags.visuals_names
-
-    end
-
-end
-
-return ESP
+print("WINDOW CREATED")
