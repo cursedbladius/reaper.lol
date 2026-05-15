62d2eaf45782880ee0fd6f8aa76df4caefea586e
777 <777@NAN>
2026-05-15 20:38:33 +0300

    lowercase folder names for cloudflare pages

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 local LoadStart = os.clock()
 local Library = loadstring(game:HttpGet("https://reaper-lol.pages.dev/ui/library.lua"))()
 
-local ESP = loadstring(game:HttpGet("https://reaper-lol.pages.dev/features/Visuals/esp.lua"))()
+local ESP = loadstring(game:HttpGet("https://reaper-lol.pages.dev/features/visuals/esp.lua"))()
 ESP:Initialize()
 
-local ToolModifier = loadstring(game:HttpGet("https://reaper-lol.pages.dev/features/Visuals/tool_modifier.lua"))()
+local ToolModifier = loadstring(game:HttpGet("https://reaper-lol.pages.dev/features/visuals/tool_modifier.lua"))()
 
 local GameRegistry = loadstring(game:HttpGet("https://reaper-lol.pages.dev/games/registry.lua"))()
 local DaHoodAdapter = loadstring(game:HttpGet("https://reaper-lol.pages.dev/games/da_hood.lua"))()
