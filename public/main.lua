5d4cf650856410175b7ae22e300dab3ca8dbd027
199 <cursedbladi@gmail.com>
2026-05-15 16:33:42 +0300

    Update main.lua

--- public/main.lua
+++ public/main.lua
@@ -30,65 +30,72 @@ local SilentAimSection = CombatTab:Section({Name = "Silent Aim", Side = 1})
 local TargetSection = CombatTab:Section({Name = "Target", Side = 2})
 local WeaponSection = CombatTab:Section({Name = "Weapon", Side = 2})
 
--- Visuals Tab - ESP Section (Left Side)
+-- Visuals Tab - Left Side (ESP Section)
 local ESPSection = VisualsTab:Section({Name = "ESP", Side = 1})
 
--- ESP Sub-tabs: Enemies | Teammates | Local
-local EnemiesTab = ESPSection:Subtab({Name = "Enemies"})
-local TeammatesTab = ESPSection:Subtab({Name = "Teammates"})
-local LocalTab = ESPSection:Subtab({Name = "Local"})
-
--- Enemies Tab Content
-EnemiesTab:Toggle({Name = "Masterswitch", Flag = "ESPEnabled", Default = false, Callback = function(Value)
+-- ESP Toggles with Colorpickers
+ESPSection:Toggle({Name = "Masterswitch", Flag = "ESPEnabled", Default = false, Callback = function(Value)
     ESP:Toggle(Value)
 end}):Colorpicker({Name = "", Flag = "ESPColor", Default = Color3.fromRGB(255, 0, 0)})
 
-EnemiesTab:Toggle({Name = "Box", Flag = "ESPBox", Default = true, Callback = function(Value)
+ESPSection:Toggle({Name = "Box", Flag = "ESPBox", Default = true, Callback = function(Value)
     ESP:SetSetting("Box", Value)
 end}):Colorpicker({Name = "", Flag = "ESPBoxColor", Default = Color3.fromRGB(255, 255, 255), Callback = function(Value)
     ESP:SetSetting("BoxColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Name", Flag = "ESPName", Default = true, Callback = function(Value)
+local NameTypeDropdown
+ESPSection:Toggle({Name = "Name", Flag = "ESPName", Default = true, Callback = function(Value)
     ESP:SetSetting("Name", Value)
+    if NameTypeDropdown then
+        NameTypeDropdown:SetVisibility(Value)
+    end
 end}):Colorpicker({Name = "", Flag = "ESPNameColor", Default = Color3.fromRGB(255, 255, 255), Callback = function(Value)
     ESP:SetSetting("NameColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Health-bar", Flag = "ESPHealthBar", Default = true, Callback = function(Value)
+NameTypeDropdown = ESPSection:Dropdown({Name = "Name Type", Flag = "ESPNameType", Default = "DisplayName", Items = {"DisplayName", "Username"}, Callback = function(Value)
+    if Value == nil then
+        NameTypeDropdown:Set("DisplayName")
+        return
+    end
+    ESP:SetSetting("NameType", Value)
+end})
+
+ESPSection:Toggle({Name = "Health-bar", Flag = "ESPHealthBar", Default = true, Callback = function(Value)
     ESP:SetSetting("HealthBar", Value)
 end}):Colorpicker({Name = "", Flag = "ESPHealthColor", Default = Color3.fromRGB(0, 255, 0), Callback = function(Value)
     ESP:SetSetting("HealthColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Distance", Flag = "ESPDistance", Default = false, Callback = function(Value)
+ESPSection:Toggle({Name = "Distance", Flag = "ESPDistance", Default = false, Callback = function(Value)
     ESP:SetSetting("Distance", Value)
 end}):Colorpicker({Name = "", Flag = "ESPDistanceColor", Default = Color3.fromRGB(255, 255, 255), Callback = function(Value)
     ESP:SetSetting("DistanceColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Skeleton", Flag = "ESPSkeleton", Default = false, Callback = function(Value)
+ESPSection:Toggle({Name = "Skeleton", Flag = "ESPSkeleton", Default = false, Callback = function(Value)
     ESP:SetSetting("Skeleton", Value)
 end}):Colorpicker({Name = "", Flag = "ESPSkeletonColor", Default = Color3.fromRGB(255, 255, 255), Callback = function(Value)
     ESP:SetSetting("SkeletonColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Chams", Flag = "ESPChams", Default = false, Callback = function(Value)
+ESPSection:Toggle({Name = "Chams", Flag = "ESPChams", Default = false, Callback = function(Value)
     ESP:SetSetting("Chams", Value)
 end}):Colorpicker({Name = "", Flag = "ESPChamsColor", Default = Color3.fromRGB(255, 0, 0), Callback = function(Value)
     ESP:SetSetting("ChamsColor", Value)
 end})
 
-EnemiesTab:Toggle({Name = "Equipped Tool", Flag = "ESPTool", Default = false, Callback = function(Value)
+ESPSection:Toggle({Name = "Equipped Tool", Flag = "ESPTool", Default = false, Callback = function(Value)
     ESP:SetSetting("Tool", Value)
 end}):Colorpicker({Name = "", Flag = "ESPToolColor", Default = Color3.fromRGB(255, 255, 255), Callback = function(Value)
     ESP:SetSetting("ToolColor", Value)
 end})
 
--- Visuals Tab - Options Section (Right Side)
+-- Visuals Tab - Right Side (Options Section)
 local OptionsSection = VisualsTab:Section({Name = "Options", Side = 2})
 
-OptionsSection:Slider({Name = "Render Distance", Min = 100, Max = 5000, Default = 1000, Decimals = 0, Flag = "ESPDistance", Callback = function(Value)
+OptionsSection:Slider({Name = "Render Distance", Min = 100, Max = 5000, Default = 1000, Decimals = 1, Suffix = " studs", Flag = "ESPDistanceSlider", Callback = function(Value)
     ESP:SetSetting("MaxDistance", Value)
 end})
 
@@ -96,7 +103,7 @@ OptionsSection:Dropdown({Name = "ESP Type", Flag = "ESPType", Default = "Dynamic
     ESP:SetSetting("BoxType", Value)
 end})
 
-OptionsSection:Dropdown({Name = "Name Font", Flag = "ESPNameFont", Default = "Default (Tahoma)", Items = {"Default (Tahoma)", "Bold", "Italic", "Source Sans"}, Callback = function(Value)
+OptionsSection:Dropdown({Name = "Name Font", Flag = "ESPNameFont", Default = "Tahoma", Items = {"Tahoma", "Monospace"}, Callback = function(Value)
     ESP:SetSetting("Font", Value)
 end})
 
@@ -110,7 +117,7 @@ OptionsSection:Toggle({Name = "Enemy Only", Flag = "ESPEnemyOnly", Default = tru
     ESP:SetSetting("TeamCheck", Value)
 end})
 
--- Visuals Tab - Extras Section (Right Side)
+-- Visuals Tab - Right Side (Extras Section)
 local ExtrasSection = VisualsTab:Section({Name = "Extras", Side = 2})
 
 ExtrasSection:Toggle({Name = "Hitsound", Flag = "Hitsound", Default = false})
@@ -123,6 +130,17 @@ local WalkSection = MovementTab:Section({Name = "Walk", Side = 1})
 local FlySection = MovementTab:Section({Name = "Fly", Side = 1})
 local JumpSection = MovementTab:Section({Name = "Jump", Side = 2})
 local SpeedSection = MovementTab:Section({Name = "Speed", Side = 2})
+
+-- Hook ESP unload into Library unload
+local OriginalUnload = Library.Unload
+Library.Unload = function(self)
+    -- Unload ESP first
+    if ESP then
+        ESP:Unload()
+    end
+    -- Call original unload
+    OriginalUnload(self)
+end
 
 -- Initialization
 Library:Notification(string.format("reaper.lol loaded in %.4f seconds", os.clock() - LoadStart), 5, Library.Theme.Accent, {"rbxassetid://135757045959142", Color3.fromRGB(149, 255, 139)})
